da179c0ae57b503a81e155ce285bb8f3d60e0e2e
Griffin <griffinq178@gmail.com>
2018-11-12 21:15:08 -0500

    Comments

--- GenericRecord.cpp
+++ GenericRecord.cpp
@@ -1,10 +1,18 @@
+/*
+CS 216 Project 03
+By Griffin Quarles
+*/
+
 #include "GenericRecord.h"
 
+//setRecord returns nothing and takes 1 arguement
 template <class Type>
 void GenericRecord<Type>::setRecord(Type r) {
+    //Set record to given arguement
     record = r;
 }
 
+//getRecord returns Type from template and takes no arguements
 template <class Type>
 Type GenericRecord<Type>::getRecord() {
     return record;
--- GenericRecord.h
+++ GenericRecord.h
@@ -1,13 +1,20 @@
+/*
+CS 216 Project 03
+By Griffin Quarles
+*/
+
 #pragma once
 #include <iostream>
 #include <string>
 
+//Create template class to store multiple types
 template <class Type>
 class GenericRecord {
     private:
     Type record;
 
     public:
+    //Accessor and mutator functions
     Type getRecord();
     void setRecord(Type r);
 };
